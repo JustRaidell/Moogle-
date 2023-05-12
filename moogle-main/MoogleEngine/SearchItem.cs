3b63c424cd760aca13d3feab78f572d0380b2d38
JustRaidell <129128894+JustRaidell@users.noreply.github.com>
2023-05-11 22:14:58 -0400

    Update SearchItem.cs

--- moogle-main/MoogleEngine/SearchItem.cs
+++ moogle-main/MoogleEngine/SearchItem.cs
@@ -1,7 +1,9 @@
 namespace MoogleEngine;
 
+//Clase que guarda los items que se mostaran en pantalla cuando se realice la busqueda
 public class SearchItem
 {
+    //Constructor.Toma Titulo del documento, Fragmento del documento que se asemeja a la query, y valor del documento respecto a la query
     public SearchItem(string title, string snippet, float score)
     {
         this.Title = title;
@@ -14,4 +16,3 @@ public class SearchItem
     public string Snippet { get; private set; }
 
     public float Score { get; private set; }
-}
