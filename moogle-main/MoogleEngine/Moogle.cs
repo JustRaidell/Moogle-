6d7c79129ce2168d47a0a63b609da82604c4d0ae
JustRaidell <129128894+JustRaidell@users.noreply.github.com>
2023-05-11 22:13:31 -0400

    Update Moogle.cs

--- moogle-main/MoogleEngine/Moogle.cs
+++ moogle-main/MoogleEngine/Moogle.cs
@@ -1,15 +1,21 @@
-﻿namespace MoogleEngine;
+namespace MoogleEngine;
 
 public class Moogle
 {
+    
     public static SearchResult Query(string query)
     {
         // Modifique este método para responder a la búsqueda
+
+        //Con la query ya introducida, normalizo la query
         query = Methods.Normalize(query);
 
+        //Array de AuxItems, más detalles en la definicion de la clase en su correspondiente en el .cs
+        //Creo un array de los Titulos y los textos de los documentos tal y como estan en la carpeta.  
         AuxItem[] non_normalized_files;
         non_normalized_files = Methods.ReadFiles();
 
+        //Este otro Array guarda los Titulos y textos de los documentos ya normalizados 
         AuxItem[] files;
         files = Methods.ReadFiles();
         for (int i = 0; i < files.Length; i++)
@@ -17,19 +23,26 @@ public class Moogle
             files[i] = new AuxItem(files[i].Title, Methods.Normalize(files[i].Text));
         }
 
+        //Diccionario universo, le asigna a cada palabra un numero entero
         Dictionary<string, int> universo = new Dictionary<string, int>();
+        //Matriz de documentos;palabras.Guarda en cada posicion la relevancia de la palabra en el documento 
         List<List<double>> vocabulario = new List<List<double>>();
 
+        //Lleno mi diccionario y mi matriz
         var aux = Methods.Initialize(0, universo, vocabulario, files);
         universo = aux.Item1;
         vocabulario = aux.Item2;
 
+        //Array que guarda el IDF de cada palabra
         double[] global_idf = Methods.IDF(vocabulario, universo, files);
+
+        //Con esto voy llenando la matriz con los TF*IDF de cada palabra en cada documento
         for (int i = 0; i < vocabulario.Count; i++)
         {
             vocabulario[i] = Methods.TF_IDF(files[i].Text, universo, vocabulario[i], global_idf);
         }
 
+        //Esta lista guarda el equivalente a la query en mi universo de palabras. O sea guarda el TF*IDF de la query  
         List<double> vect_query = new List<double>();
         for (int i = 0; i < vocabulario[0].Count; i++)
         {
@@ -37,108 +50,35 @@ public class Moogle
         }
         vect_query = (Methods.TF_IDF(query, universo, vect_query, global_idf));
 
+        //Este array score guarda la similitud de cada documento con la query. Mientras mayor score, mas parecido el documento
         double[] score = new double[vocabulario.Count];
         for (int i = 0; i < vocabulario.Count; i++)
         {
             score[i] = Methods.CosSimilarity(vocabulario[i], vect_query);
         }
 
+        //Array de SearchItem que se imprime en pantalla
         SearchItem[] items = new SearchItem[files.Length];
 
-        string[] splitted_query = query.Split();
-        double[] query_max_relevance = new double[files.Length];
-        string[] query_most_relevant_word = new string[files.Length];
-        Array.Fill(query_max_relevance, 0);
+        //Lista de los scores de los documentos y sus posiciones, ordenados de mayor a menor por score
+        List <Tuple <double, int>> docs_order = new List<Tuple<double, int>> ();
         for (int i = 0; i < files.Length; i++)
         {
-            query_most_relevant_word[i] = non_normalized_files[i].Text.Split()[0];
+            docs_order.Add(Tuple.Create(score[i], i)); 
         }
-        for (int i = 0; i < splitted_query.Length; i++)
-        {
-            for (int j = 0; j < files.Length; j++)
-            {
-                if (!universo.ContainsKey(splitted_query[i])) continue;
-                if (query_max_relevance[j] < vocabulario[j][universo[splitted_query[i]]])
-                {
-                    query_most_relevant_word[j] = splitted_query[i];
-                }
-            }
-        }
+        docs_order.Sort((x, y) => y.Item1.CompareTo(x.Item1));
 
-        string[] full_snippet = new string[items.Length];
-        Array.Fill(full_snippet, "");
-        for (int i = 0; i < items.Length; i++)
-        {
-            string[] pre_snippet = non_normalized_files[i].Text.Split(" , \n");
-            for (int j = 0; j < pre_snippet.Length; j++)
-            {
-                int m = 0;
-                int n = 0;
-                if (pre_snippet[j] == query_most_relevant_word[i])
-                {
-                    if (j - 10 < 0)
-                    {
-                        m = 0;
-                        n = j + 10;
-                    }
-                    else if (j + 10 >= pre_snippet.Length)
-                    {
-                        m = j - 10;
-                        n = pre_snippet.Length - 1;
-                    }
-                    else
-                    {
-                        m = j - 10;
-                        n = j + 10;
-                    }
-                }
-
-                for (int k = m; k <= n; k++)
-                {
-                    full_snippet[i] = full_snippet[i] + pre_snippet[k];
-                }
-            }
-        }
-
-        /*
-                bool[] scored = new bool [items.Length];
-                Array.Fill (scored,false);
-                for (int i = 0; i < items.Length; i++)
-                {
-                    double major_score = 0;
-                    int pos = -1;
-                    for (int j = 0; j < items.Length; j++)
-                    {
-                        if(major_score < score[j] && !scored[j]){
-                            major_score = score[j];
-                            pos = j;
-                        }
-                    }
-        */
-        //            if(score[pos] != 0)
+        //Esto llena el array que se imprimira en pantalla
         for (int i = 0; i < items.Length; i++)
         {
             //if(score[i] != 0)
-            items[i] = new SearchItem(non_normalized_files[i].Title, full_snippet[i], ((float)score[i]));
+            items[i] = new SearchItem(non_normalized_files[docs_order[i].Item2].Title, non_normalized_files[docs_order[i].Item2].Text, ((float)score[docs_order[i].Item2]));
         }
 
-        //            scored[pos] = true;
-
-        return new SearchResult(items, "¿No ecuentras lo que buscas? Revisa bien la escritura y ortografía");
+        return new SearchResult(items, "¿No encuentras lo que buscas? Revisa bien la escritura y ortografía");
     }
-
-        /*       
-                t = 0;
-                for (int i = 0; i < valor.Length; i++)
-                {
-                    if (valor[i] == 0) continue;
-                    else
-                    {
-                        items[t] = new SearchItem(non_normalized_files[i].Title, non_normalized_files[i].Text, ((float)valor[i]));
-                        t++;
-                    }
-                }
-        */
-
 }
+
+
+
 
